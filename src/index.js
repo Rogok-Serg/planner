8c954328dda97e50b34de95abcf3144bf5e2c783
Rogok-Serg <rogoksv@gmail.com>
2023-12-04 02:37:50 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -5,17 +5,14 @@ import App from 'App';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
-// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={<p>Loading...</p>} persistor={persistor}> */}
       <BrowserRouter basename="/planner">
         {' '}
         <App />
       </BrowserRouter>
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,36 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { todoReducers } from './todoReducer';
 
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-
-// const todoPersistConfig = {
-//   key: 'todoStorage',
-//   storage,
-//   whitelist: ['tasks'],
-// };
 
 export const store = configureStore({
   reducer: {
     todoStorage: todoReducers,
-    //     auth: persistReducer(authPersistConfig, authReducer),
-    //     // ),
   },
-  // middleware: getDefaultMiddleware =>
-  //   getDefaultMiddleware({
-  //     serializableCheck: {
-  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //     },
-  //   }),
 });
 
-// export const persistor = persistStore(store);
